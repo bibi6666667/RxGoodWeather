030e9ba37aea37146d5970f9f4dbd0f9458097fc
Bibi <bibi6666667@kakao.com>
2022-12-28 11:33:46 +0900

    70. Building User Interface for the App

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Bibi on 2022/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temeratureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
